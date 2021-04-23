e093d6b300d9b5c5d62c45d55f4bdf50d9e33580
Elz Neto <eneto@cfav.nc.com>
2021-04-23 16:20:41 -0400

    first commit

---
+++ PaymentUIStripe.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using PaymentUIStripe.Controls;
+using PaymentUIStripe.iOS.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Entry), typeof(CustomEntryRenderer), new[] { typeof(CustomVisual) })]
+
+namespace PaymentUIStripe.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
---
+++ PaymentUIStripe/Views/CreditCardPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using PaymentUIStripe.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaymentUIStripe.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(false)]
+    public partial class CreditCardPage : ContentPage
+    {
+        public CreditCardPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new CreditCardPageViewModel();
+        }
+    }
+}
